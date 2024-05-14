be703b8ae2cbf560f7296c150af972f4135ed702
alvarezvald <m.alvarez@vald.com>
2024-05-14 11:24:14 +1000

    fixed create leaderboard 15.1

--- src/tests/15.0 Leaderboard/15.1-createLeaderboard.test.ts
+++ src/tests/15.0 Leaderboard/15.1-createLeaderboard.test.ts
@@ -24,41 +24,22 @@ test.beforeEach(async ({ page }) => {
 test('15.1.1 Create a leaderboard on VALD Hub @trainingprogram @regression', async ({
     page,
 }) => {
+    await page.getByRole('link', { name: 'Dashboard' }).click();
+    await page.getByLabel('Close').click();
+    await page.locator('header svg').nth(1).click();
+    await page.getByRole('button', { name: 'Leaderboard' }).click();
+    await page.getByLabel('Create Leaderboard').click();
+    await page.getByPlaceholder('Enter Leaderboard Name').click();
+    await page.getByPlaceholder('Enter Leaderboard Name').fill('ForceDecks Leaderboard Automation');
+    await page.locator('.stack > .select-container > .final-form-field-container > .final-form-field > .form-field-input > .react-select > .react-select__control > .react-select__value-container > .react-select__input-container').click();
+    await page.getByText('Countermovement Jump', { exact: true }).click();
+    await page.locator('.stack > div:nth-child(2) > .final-form-field-container > .final-form-field > .form-field-input > .react-select > .react-select__control > .react-select__value-container > .react-select__input-container').click();
+    await page.getByText('Contraction Time', { exact: true }).click();
+    await page.getByTestId('leaderboard-configuration-form').getByLabel('Create').click();
 
-    await test.step('Navigating to Configuration Tab from the left hand side menu', async () => {
-        await page.getByRole('link', { name: 'Dashboard' }).click();
-
-        await page.getByLabel('Close').click();
-        await page.locator('header svg').nth(1).click();
-        await page.getByRole('button', { name: 'Leaderboard' }).click();
-        await page.getByLabel('Create Leaderboard').click();
-        await page.getByPlaceholder('Enter Leaderboard Name').click();
-        await page.getByPlaceholder('Enter Leaderboard Name').fill('Test Leaderboard');
-        await page.getByTestId('testTypeId').locator('svg').click();
-        await page.getByTestId('testTypeId').locator('svg').click();
-        await page.getByTestId('testTypeId').locator('svg').click();
-        await page.getByText('Countermovement Rebound Jump', { exact: true }).click();
-        await page.getByTestId('metricId').locator('svg').click();
-        await page.getByText('First Flight Time', { exact: true }).click();
-        await page.locator('div:nth-child(2) > .react-select > .react-select__control > .react-select__value-container > .react-select__input-container').click();
-        await page.locator('#react-select-3-option-6').click();
-        await page.getByTestId('leaderboard-configuration-form').getByLabel('Create').click();
-        await page.getByLabel('Back to Leaderboard').click();
-        await page.getByText('Test Leaderboard').click();
-        await page.getByLabel('Edit').click();
-        await page.getByPlaceholder('DD/MM/YYYY').click();
-        await page.getByText('‹').click();
-        await page.getByText('›').click();
-        await page.getByText('22').click();
-        await page.getByLabel('Save').click();
-        await page.getByLabel('Edit').click();
-        await page.getByTestId('close-button').click();
-        await page.getByLabel('Back to Leaderboard').click();
-        await page.getByTestId('actions-dropdown').getByRole('button').first().click();
-        await page.getByLabel('Delete').click();
-        await page.getByTestId('modal').getByLabel('Delete').click();
-
-    });
+    // Click on back to view all leaderboards
+    await page.getByText('Back to Leaderboard').click();
+    await page.getByText('ForceDecks Leaderboard', { exact: true }).isVisible();
 
     await test.step('Create training program here', async () => {
 
